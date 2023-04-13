9b539a8c2835b93b746947c635e6f4cfe9f04a2d
k4rpV <101994618+k4rpV@users.noreply.github.com>
2023-04-13 17:54:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,4 @@
-import pandas as pd
-import numpy as np
-from scipy.stats import norm
+from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 953761952
 
@@ -8,6 +6,4 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    p1 = x_success / x_cnt
-    p2 = y_success / y_cnt
-    return p2 - p1 < norm.ppf(0.03, loc=0, scale=np.sqrt(p1 * (1-p1) / x_cnt + p2 * (1 - p2) / y_cnt))
+    return proportions_ztest(count=[x_success, y_success], nobs=[x_cnt, y_cnt], alternative='smaller')[1] > 0.03
